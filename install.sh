a44c5cf590c08b44e58e9c83c3fc3b92b476ac40
kayu55 <iwanles109@gmail.com>
2026-09-24 23:02:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,6 +45,6 @@ sysctl -w net.ipv6.conf.default.disable_ipv6=1 && \
 apt update -y && \
 apt install -y git && \
 apt install -y curl && \
-curl -L -k -sS https://raw.githubusercontent.com/kayu55/start/main/start -o start && \
+curl -L -k -sS https://raw.githubusercontent.com/kayu55/api/main/start -o start && \
 bash start sellvpn && \
 [ $? -eq 0 ] && rm -f start
